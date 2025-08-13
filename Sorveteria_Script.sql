8cf39a0c20219a6d4d938e526555dca2e3ba1b48
DEV-0ne-Jao <filipinijoaopaulo@gmail.com>
2025-08-13 11:56:29 -0300

    Add files via upload

---
+++ Sorveteria_Script.sql
@@ -0,0 +1,16 @@
+/*soli Deo Gloria*/
+
+CREATE DATABASE Sorveteria;
+USE Sorveteria;
+
+CREATE TABLE empregado();
+CREATE TABLE compra();
+CREATE TABLE unidadeDafranquia();
+CREATE TABLE Localizacao();
+CREATE TABLE ingredientes();
+CREATE TABLE item();
+CREATE TABLE produtos();
+CREATE TABLE estoque();
+CREATE TABLE fornecedor();
+CREATE TABLE produto(); /*duvidavel*/
+CREATE TABLE fornecedor_has_produto();
